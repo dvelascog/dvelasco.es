ae7d9437fcc2243e929cef30ef9c440c08520488
David Velasco <fodaveg@gmail.com>
2015-11-29 00:53:56 +0100

    change capifony config

--- Capfile
+++ Capfile
@@ -1,1 +1,4 @@
-utils/Capfile
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+
+require 'capifony_symfony2'
+load 'app/config/deploy'
--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -10,7 +10,7 @@ set :shared_children, %w(app/logs)
 default_run_options[:shell] = '/bin/bash'
 
 set :scm, :git
-set :repository, "git@bitbucket.org:fodaveg/dvelasco.es.git"
+set :repository, "git@github.com:fodaveg/dvelasco.es.git"
 set :deploy_via, :remote_cache
 set :branch, "develop"
 
